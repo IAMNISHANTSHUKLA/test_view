c93258b96fee69d0779d66bb6c9631801e93bf09
Deep <dp124551634@gmail.com>
2024-10-13 18:18:13 +0530

    Configure RDBMS in product-service to use JPA-repository

--- products/src/main/java/com/ecommerce/products/controller/ProductController.java
+++ products/src/main/java/com/ecommerce/products/controller/ProductController.java
@@ -3,10 +3,12 @@ package com.ecommerce.products.controller;
 import com.ecommerce.products.dao.InventoryEs;
 import com.ecommerce.products.dao.InventoryMain;
 import com.ecommerce.products.models.ProductsEs;
+import com.ecommerce.products.models.ProductsMain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,17 +23,27 @@ public class ProductController {
 
     @GetMapping("")
     public ResponseEntity<?> getProducts(@RequestParam(required = false, name = "q") String query) {
+        List<ProductsMain> productsMainList = inventoryMain.findAll();
         if (query != null) {
-            return ResponseEntity.ok(inventoryEs.findAll().stream()
-                    .filter(product -> product.getName().toLowerCase().contains(query.toLowerCase()) || product.getDescription().toLowerCase().contains(query.toLowerCase()))
-                    .toArray());
+            List<ProductsMain> filteredProducts = new ArrayList<>();
+            for (ProductsMain product : productsMainList) {
+                if (product.getName().toLowerCase().contains(query.toLowerCase()) || product.getDescription().toLowerCase().contains(query.toLowerCase())){
+                    filteredProducts.add(product);
+                }
+            }
+            return ResponseEntity.ok(filteredProducts);
         }
-        return ResponseEntity.ok(inventoryEs.findAll());
+        return ResponseEntity.ok(productsMainList);
     }
 
     @PostMapping("")
     public ResponseEntity<?> addProducts(@RequestBody List<ProductsEs> productsEsList) {
-        inventoryMain.insertData(productsEsList);
+        List<ProductsMain> productsMainList = new ArrayList<>();
+        for (ProductsEs product : productsEsList) {
+            productsMainList.add(new ProductsMain(product.getId(), product.getName(), product.getDescription(), product.getPrice()));
+        }
+        inventoryMain.saveAll(productsMainList);
+//        inventoryMain1.insertData(productsEsList);
         inventoryEs.saveAll(productsEsList);
         return ResponseEntity.ok(productsEsList);
     }
--- products/src/main/java/com/ecommerce/products/dao/InventoryMain.java
+++ products/src/main/java/com/ecommerce/products/dao/InventoryMain.java
@@ -1,21 +1,7 @@
 package com.ecommerce.products.dao;
 
-import com.ecommerce.products.models.ProductsEs;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import com.ecommerce.products.models.ProductsMain;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-@Component
-public class InventoryMain {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    public void insertData(List<ProductsEs> productsEsList){
-        for (ProductsEs product : productsEsList) {
-            jdbcTemplate.update("INSERT INTO products (id, name, description, price) VALUES (?, ?, ?, ?)",
-                    product.getId(), product.getName(), product.getDescription(), product.getPrice());
-        }
-    }
+public interface InventoryMain extends JpaRepository<ProductsMain, Integer> {
 }
